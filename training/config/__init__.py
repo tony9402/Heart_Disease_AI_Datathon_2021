c1e6944912be218823edddc730cebf8922d7ff86
tony9402 <tony9402@naver.com>
2021-11-30 15:04:03 +0000

    [ADD] config __init__ file

---
+++ training/config/__init__.py
@@ -0,0 +1,17 @@
+from training.utils.Config import toConfig
+import json
+import os
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+jsons = [os.path.join(dirname, json_file) for json_file in os.listdir(dirname) if json_file.endswith('.json')]
+configs = {os.path.basename(path)[:-5]: path for path in jsons }
+
+def load_config(name):
+    if name not in configs:
+        raise KeyError(f"Not exists '{name}' Config")
+
+    with open(configs[name], 'r') as f:
+        config = toConfig(json.load(f))
+        f.close()
+
+    return config
