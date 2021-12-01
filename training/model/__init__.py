136923af3b49d8853e1432cc6d93c0dfc7baf84b
tony9402 <tony9402@naver.com>
2021-12-01 19:53:31 +0000

    [ADD] Add new model

--- training/model/__init__.py
+++ training/model/__init__.py
@@ -3,12 +3,14 @@ from .swinT import SwinTransformer
 from .DeepLabV3 import DeepLabV3plus_ResNet50
 from .DeepLabV3 import DeepLabV3plus_ResNet101
 from .DeepLabV3 import DeepLabV3plus_HRNetV2_32
+from .closedUNet import ClosedUNet
 
 _models = [
     UNet,
     DeepLabV3plus_ResNet50,
     DeepLabV3plus_ResNet101,
-    DeepLabV3plus_HRNetV2_32
+    DeepLabV3plus_HRNetV2_32,
+    ClosedUNet
 ]
 _models_dict = {v.__name__: v for v in _models}
 
